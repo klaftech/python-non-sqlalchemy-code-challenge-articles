d9e473cbadeeca1d79c82499b2ee0aab61fcbc92
Elchonon <ekn400@gmail.com>
2024-12-17 17:25:20 -0500

    ORM done

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,18 +1,64 @@
 class Article:
+    all = []
+    
+    def __repr__(self):
+        return f"<Article title: {self.title}, author: {self.author.name}, magazine: {self.magazine.name}>"
+    
     def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+        self.__class__.all.append(self)
+
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self,value):
+        if type(value) == Author:
+            self._author = value
+
+    @property
+    def magazine(self):
+        return self._magazine
+    
+    @magazine.setter
+    def magazine(self,value):
+        if type(value) == Magazine:
+            self._magazine = value
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self,value):
+        if isinstance(value, str) and 5 <= len(value) <= 50 and not hasattr(self,"title"):
+            self._title = value
+
         
 class Author:
+    def __repr__(self):
+        return f"<Author name: {self.name}>"
+    
     def __init__(self, name):
-        self.name = name
+        self._name = name
+        
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self,value):
+        if isinstance(value, str) and len(value) >= 0 and not hasattr(self,"name"):
+            self._name = value
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        return list(set(article.magazine for article in self.articles()))
 
     def add_article(self, magazine, title):
         pass
@@ -21,15 +67,36 @@ class Author:
         pass
 
 class Magazine:
+    def __repr__(self):
+        return f"<Magazine name: {self.name}, category: {self.category}>"
+
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        self._name = name
+        self._category = category
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self,value):
+        if isinstance(value, str) and 2 <= len(value) <= 16:
+            self._name = value
+    
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self,value):
+        if isinstance(value, str) and len(value) > 0:
+            self._category = value
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set(article.author for article in self.articles()))
 
     def article_titles(self):
         pass
--- lib/debug.py
+++ lib/debug.py
@@ -8,6 +8,15 @@ from classes.many_to_many import Magazine
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
+    magazine_1 = Magazine("Vogue", "Fashion")
+    magazine_2 = Magazine("AD", "Architecture")
+
+    author_1 = Author("Carry Bradshaw")
+    author_2 = Author("Nathaniel Hawthorne")
+    
+    article_1 = Article(author_1, magazine_1, "How to wear a tutu with style")
+    article_2 = Article(author_2, magazine_1, "Dating life in NYC")
+
 
     # don't remove this line, it's for debugging!
     ipdb.set_trace()
